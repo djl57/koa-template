da5dcf4037e6af7370e367cfb6ec0334cb26ad8c
djl57 <18367345931@163.com>
2019-08-27 16:43:28 +0800

    co

--- app.js
+++ app.js
@@ -6,48 +6,9 @@ const app = new Koa();
 app.use(cors());
 app.use(bodyParser());
 
-// const mysql = require("mysql");
-// const connection = mysql.createConnection({
-//   host: "localhost",
-//   user: "root",
-//   password: "12345678",
-//   database: "my_all_datas"
-// });
-// connection.connect(err => {
-//   if (err) {
-//     console.log("数据库连接失败：", err);
-//     return;
-//   }
-//   console.log("数据库连接成功，连接id为：", connection.threadId);
-// });
-// connection.query('SELECT * FROM `books` WHERE `author` = "David"', function (error, results, fields) {
-//   // error will be an Error if one occurred during the query
-//   // results will contain the results of the query
-//   // fields will contain information about the returned results fields (if any)
-// });
-// connection.end(err => {
-//   if (err) {
-//     console.log("数据库已断开，出现错误：", err);
-//     return;
-//   }
-//   console.log("数据库已断开");
-// });
-
-// const router = require("koa-router")();
-
-// router.get("/", async ctx => {
-//   // console.log(result)
-//   console.log(ctx.url);
-//   ctx.body = { code: 0, msg: "行程添加成功" };
-// });
-
-// app.use(router.routes());
-// app.use(router.allowedMethods());
-
 const example01 = require('./interface/example01')
 app.use(example01.routes());
 app.use(example01.allowedMethods());
-
 
 app.listen(3001, () => {
   console.log("http://localhost:3001");
--- interface/example01/index.js
+++ interface/example01/index.js
@@ -1,8 +1,10 @@
 const router = require("koa-router")();
-// const connectMysql = require('../../db')
 const partName = "/example01";
 const DB = require("../../db/index");
-
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+const jwt = require("jsonwebtoken");
+const secretOrPrivateKey = "secret";
 /**
  * @func
  * @todo nope
@@ -24,24 +26,53 @@ const DB = require("../../db/index");
  * @returnParam {object} ctx.response.message 请求返回信息
  * @returnParam {object} ctx.response.body 请求返回主体
  */
-
+/* 
+  300 用户输入内容报错
+  500 数据库报错
+  200 正常
+  600 jsonwebtoken报错
+*/
 router.post(partName + "/register", async ctx => {
   const body = ctx.request.body;
-  const { useremail } = body;
+  const { username, useremail, password } = body;
   try {
-    const result = await DB.sqlQuery("SELECT * FROM `user` WHERE `useremail` = ?", [useremail]);
+    const result = await DB.sqlQuery("SELECT * FROM `user` WHERE `useremail` = ?", [useremail]); /* 查 */
     if (result.length !== 0) {
       ctx.body = { code: 300, msg: "邮箱已存在" };
       return;
     }
     try {
-      await DB.sqlQuery("INSERT INTO user SET ?", { ...body });
+      const hashPassword = await bcrypt.hash(password, saltRounds);
+      await DB.sqlQuery("INSERT INTO user SET ?", { username, useremail, password: hashPassword }); /* 增 */
       ctx.body = { code: 200, msg: "用户添加成功" };
     } catch (error) {
       ctx.body = { code: 500, msg: "用户添加失败" };
     }
   } catch (error) {
     ctx.body = { code: 500, msg: "邮箱验证失败" };
+  }
+});
+
+router.post(partName + "/login", async ctx => {
+  const body = ctx.request.body;
+  const { useremail, password } = body;
+  const result = await DB.sqlQuery("SELECT * FROM `user` WHERE `useremail` = ?", [useremail]);
+  if (result.length === 0) {
+    ctx.body = { code: 300, msg: "用户不存在" };
+    return;
+  }
+  const match = await bcrypt.compare(password, result[0].password);
+  if (match) {
+    try {
+      /* jwt.sign("规则", "加密名字", "过期时间", "箭头函数") */
+      const rule = { userid: result[0].userid, username: result[0].username };
+      const token = await jwt.sign(rule, secretOrPrivateKey, { expiresIn: "1h" });
+      ctx.body = { code: 200, msg: "登录成功！", token: "example01-" + token };
+    } catch (error) {
+      ctx.body = { code: 600, msg: "token 生成失败" };
+    }
+  } else {
+    ctx.body = { code: 300, msg: "密码错误" };
   }
 });
 
